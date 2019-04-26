44d8cfd923f42c1d895b1e85887ed4050be86a97
Leonardo Av <leoavfag@gmail.com>
2019-04-25 23:53:31 -0300

    Adicionada parte de cruzamento
    Adicionei parte de cruzamento e a implementei na função principal.

--- AlgGen.py
+++ AlgGen.py
@@ -5,12 +5,12 @@ import math
 
 tamP = 20
 Dim = 2
-TamGer = 10 #Quantas gerações
+TamGer = 100 #Quantas gerações
 Ch_Mut = 0.3 #Chance de mutação
 max_ = 5.12
 min_ = -5.12
 A = 10 #Constante da função rastrigin
-
+chancec = 0.9
 
 def Init():
     POPI = np.zeros((tamP,Dim)) #População INICIAL
@@ -30,7 +30,7 @@ def Init():
     for gene in range(tamP):            #Preenche a população com os genes
         POPI[gene][0]= POPconj[gene]
         POPI[gene][1]= POPconj2[gene]
-        
+    #x = np.zeros(())
     return POPI
 
 def Rastrigin(POPI,**kwargs): #calcula e retorna o valor da função e acordo com parametros recebidos
@@ -44,11 +44,11 @@ def Rastrigin(POPI,**kwargs): #calcula e retorna o valor da função e acordo co
 
 def inverte(fx):    #Função fitness, para nao ter problemas de divisao por zero
     hx = min(fx)    #Ocorre um deslocamento para tal
-    desl = 10**-0.2 - hx;
+    desl = 10**-0.2 - hx
     return 1/( fx + desl)
 
 def torneio(fx):
-    ind = np.zeros(len(fx)) #cria vetor com o tamanho do fx
+    ind = np.zeros(len(fx),dtype=int) #cria vetor com o tamanho do fx
     for i in range(len(fx)):
         i1 = random.randint(0,len(fx)-1)
         i2 = random.randint(0, len(fx)-1)
@@ -57,6 +57,22 @@ def torneio(fx):
         else:
             ind[i] = i2
     return ind
+
+def cruzamento(X,chancec):
+    l = X.shape[0]
+    c = X.shape[1]
+    Xfilho = np.zeros((l,c))
+    filho=0
+    alpha=0.9
+    while filho <l:
+        i1 = random.randint(0,l-1)
+        i2 = random.randint(0,l-1)
+        if random.random() < chancec:
+            Xfilho[filho,:] = alpha*X[i1,:] + (1-alpha)* X[i2,:]
+            filho+=1
+            Xfilho[filho,:] = alpha*X[i2,:] + (1-alpha)* X[i1,:]
+            filho+=1
+    return Xfilho
 
 
 def mutacao(X,chance): 
@@ -83,35 +99,14 @@ def mutacao(X,chance):
         else:
             Xn[i:] = X[i,:]
     return Xn
-
-def cruza(x,ind,fx): # cruza aleatoriamente os indices selecionados pelo fitness, mantem a parte inteira de uma e coloca a flutuante de outra
-    [row,col] = x.shape
-    xn = np.zeros((row,col))
-    for i in range(row):
-        i1 = random.randint(0,len(ind)-1)    #num aleatorio para pegar um indice dos selecionados(fitness) 
-        i2 = random.randint(0, len(ind)-1)   #num aleatorio para pegar outro indice dos selecionados(fitness)
-        for j in range(col):
-            #x[ind[i1]] e x[ind[i2]]
-            a = int(ind[i1])
-            b = int(ind[i2])
-            if fx[a]>fx[b]:
-                inteira = int(x[a,j]) #pega a parte inteira do primeirok
-                inteira2= int(x[b,j])
-                flutuante = x[b,j] - inteira2 #pega a parte flutuante do segundo
-                xn[i,j] = (inteira + flutuante) # soma as duas partes para criar o filho
-            else:
-                inteira = int(x[b,j]) #pega a parte inteira do primeiro
-                inteira2= int(x[a,j]) #pega a parte inteira do segundo
-                flutuante = x[a,j] - inteira2 #pega a parte flutuante do segundo
-                xn[i,j] = (inteira + flutuante) # soma as duas partes para criar o filho
     
-    return xn
-
-def Geracoes(x,z,fx,**kwargs): #Cria um numero definido de gerações por um numero definido de vezes e armazena os melhores
-    
-    vezes = kwargs.get('vezes')
+def Geracoes(**kwargs): #Cria um numero definido de gerações por um numero definido de vezes e armazena os melhores
+    x = Init()  # primeira geração
+    z = Rastrigin(x, A=10) # calcula valores rastrigin
+    fx = inverte(z)
+    vezes = kwargs.get("vezes")
     [row,col] = x.shape
-    melhores = np.zeros((vezes,col+1))
+    melhores = np.zeros((vezes,col))
     for j in range(vezes):
         for i in range(TamGer):
            
@@ -119,47 +114,39 @@ def Geracoes(x,z,fx,**kwargs): #Cria um numero definido de gerações por um num
             melhorfx = fx[melhorind]
             # torneio
             ind = torneio(fx)
-
+            Xt = np.array(x)[ind,:]
+            #print(Xt)
             # AQUI PRECISO DO ALGORITMO DE CRUZAMENTO   
-            Xc = cruza(x,ind,fx)
+            Xc = cruzamento(Xt,chancec)
 
             Xn = mutacao(Xc, Ch_Mut) #<- Xc no lugar de x 
             
             z = Rastrigin(Xn, A=10) #refaz os passos para o novo Xc
             fx = inverte(z)
+            
             auxind =  np.argmin(fx)         #Pega o indice do pior fx
             Xn[auxind, :] = x[melhorind, :] #Substitui na nova geracao aquele indice
             fx[auxind] = melhorfx
-            print("MELHOR DA INTERAÇÂO "+str(j)+" GERAÇAO "+str(i)+": "+str(x[melhorind,:])+" fx:"+str(melhorfx) )
+            #print("MELHOR DA INTERAÇÂO "+str(j)+" GERAÇAO "+str(i)+": "+str(x[melhorind,:])+" fx:"+str(melhorfx) )
+            
             x = Xn
             melhorindfin = np.argmin(z)
-            
             if melhorfx > fx[melhorindfin]:
-                melhores[j,0:2] = x[melhorind]
-                melhores[j,2] = melhorfx
+                melhores[j,:] = x[melhorind]
             else:
-                melhores[j,0:2] = x[melhorindfin]
-                melhores[j,2] = fx[melhorindfin]
+                melhores[j,:] = x[melhorindfin]
                 
             if i==TamGer-1:
                 print(melhores[j,:])
-
+        x = Init()  # primeira geração
+        z = Rastrigin(x, A=10) # calcula valores rastrigin
+        fx = inverte(z)
     return melhores
 
-x = Init()  # primeira geração
-z = Rastrigin(x, A=10) # calcula valores rastrigin
-fx = inverte(z) #calcula fitness
-
-melhores = Geracoes (x, z ,fx, vezes = 30)
+    
+melhores = Geracoes (vezes = 30)
 print("\n\n Melhores 30 de 100 em 100 gerações: \n")
 for i in range(len(melhores)):
-    print("Interação "+str(i)+"-> "+str(melhores[i,0:2])+" FX: "+str(melhores[i,2]))
-
-
-
-
-
-#print(melhores)
-
-
-
+    print("Interação "+str(i)+"-> "+str(melhores[i,:]))
+z = Rastrigin(melhores, A=10)
+print(z)
